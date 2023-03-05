7414a4d73faea6dbcd72ad2c64815d3375ba7bfc
mrileyclark <103368285+mrileyclark@users.noreply.github.com>
2023-03-05 12:57:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+// number guesser
 
+
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+ 
+
+// Write your code below:
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
+
+const compareGuesses = (userGuess, computerGuess, secretTarget) => {
+    const humanDifference = Math.abs(secretTarget - userGuess)
+    const computerDifference = Math.abs(secretTarget - computerGuess)
+    return humanDifference <= computerDifference;
+}
+
+
+const updateScore = winner => {
+  if(winner === 'human') {
+    humanScore++;
+  } else if (winner === 'computer'){
+    computerScore++;
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber++;
+}
+
+ 
